3d5bc9861c91801834d95b4ff1f2d9d850dd30db
WesleyLiaw <wesleyliaw777@gmail.com>
2023-05-30 10:50:00 -0500

    init commit, to auth

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAazaIL8UdScIXrvlvbPxP3LsEqK7OxZHA",
+  authDomain: "twitter-clone-90a39.firebaseapp.com",
+  projectId: "twitter-clone-90a39",
+  storageBucket: "twitter-clone-90a39.appspot.com",
+  messagingSenderId: "927231300169",
+  appId: "1:927231300169:web:9e8f058876be7d7ff12598"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
